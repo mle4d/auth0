f011a26ab9833e3eee6462efd1f8c1a90c10a0c4
emily <mle4d@me.com>
2019-09-05 15:48:27 -0700

    update index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import Auth0Provider from './components/Auth0Provider';
+import store from './store';
+import { Provider } from 'react-redux';
 
 render(
   <Auth0Provider
@@ -9,7 +11,9 @@ render(
     client_id="KskVK9DHW3ZJtVjYXcnoBrZn6u9R7brE"
     redirect_uri="http://localhost:7890"
   >
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Auth0Provider>,
   
   document.getElementById('root')
