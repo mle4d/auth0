3ad8defc0c335579de95cfd45d7dbb39aba5f2e4
emily <mle4d@me.com>
2019-09-05 14:05:03 -0700

    fixed teeny errors, working now!

--- src/components/Auth0Provider.js
+++ src/components/Auth0Provider.js
@@ -10,20 +10,19 @@ const DEFAULT_REDIRECT_CALLBACK = () => {
 export const Auth0Context = React.createContext();
 export const useAuth0 = () => useContext(Auth0Context);
 
-export default function Auth0Provider({ 
-  children, onRedirectCallback = DEFAULT_REDIRECT_CALLBACK, ...initOptions }) { 
+export default function Auth0Provider({ children, onRedirectCallback = DEFAULT_REDIRECT_CALLBACK, ...initOptions }) { 
   const [isAuthenticated, updateIsAuthenticated] = useState(false);
   const [user, setUser] = useState();
   const [auth0Client, setAuth0Client] = useState();
   const [loading, updateLoading] = useState(true);
 
   useEffect(() => {
-    const initAuth0 = async () => {
-      const auth0 = createAuth0Client(initOptions);
+    const initAuth0 = async() => {
+      const auth0 = await createAuth0Client(initOptions);
       setAuth0Client(auth0);
 
 
-      if(window.location.search('code=')) {
+      if(window.location.search.includes('code=')) {
         const { appState } = await auth0.handleRedirectCallback();
         onRedirectCallback(appState);
       }
