2ad77e17eededc6ef59ecf2aff8b888d2a7d6214
emily <mle4d@me.com>
2019-09-05 15:48:06 -0700

    reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import unicorn from './unicornReducer';
+
+export default combineReducers({
+  unicorn
+});
---
+++ src/reducers/unicornReducer.js
@@ -0,0 +1,6 @@
+export default function(state = [{ name: 'Sparkles' }], action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
